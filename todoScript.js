136925b68337bde68a1af8daaa7f2ef7cb5cdb00
Abdullahi Mohammed <odanwu211@gmail.com>
2023-08-22 12:12:07 +0100

    day 5 of 45 days javascript coding challenge/ building to-do list using slice and splice methods

---
+++ todoScript.js
@@ -0,0 +1,30 @@
+// TO-DO LIST PROJECT USING SLICE AND SPLICE METHODS
+//1. initialize an empty array called todoList
+
+//2. define a function calleld addItem with parameter item
+
+// use splice method to add the item to the end of the todoList array
+
+// display a message indicating that the item is added
+
+// 3.. define a function named removeItem with a parameter (index)
+
+// check if the index is within the valid range of indices in todoList array
+
+// display message indicating the item has been removed
+
+// if thne index is not valid:
+// display an error message indicating that the index is invalid and the item was not removed
+
+//4.define a function diplayList
+// display a header message display TO-DO LIST
+// loop through each item in the todoList array
+// display the item along with its corresponding index starting from 1
+
+// 5. display the current to-do list using the display list
+
+// 6. add some initial item to the todo-list using addItem function
+
+// 7. remove an item from the to-do list using the removeItem function specifying the index of the item to be removed
+
+// 8 display the updated to-do list using the display list function
